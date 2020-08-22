38f35ebfc8fe2bf66804c399b9acd21131f327ea
sedx876 <swatkins876@gmail.com>
2020-08-22 08:09:42 -0400

    added todoitem component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoItem from './components/TodoItem'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+      <input type="checkbox" />
+      <p>Placeholder</p>
+      <input type="checkbox" />
+      <p>Placeholder</p>
+      <input type="checkbox" />
+      <p>Placeholder</p>
+      <input type="checkbox" />
+      <p>Placeholder</p>
+      </div>
+    )
+  }
+}
