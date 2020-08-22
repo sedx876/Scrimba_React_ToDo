e870d252cc85073250b101eab9076709b13d4d97
sedx876 <swatkins876@gmail.com>
2020-08-22 08:17:46 -0400

    some styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class TodoItem extends Component {
   render() {
     return (
-      <div>
+      <div className="todo-item">
       <input type="checkbox" />
       <p>Placeholder</p>
       <input type="checkbox" />
